9753d1b6f72d582bcd0b61a6b23cf4a2cea48e45
Rick McGeer <rick@mcgeer.com>
2022-09-16 03:49:20 +0000

    changed file2 to file3

--- file2.cpp
+++ file2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "file2.h"
 void function_from_file2() {
-    std::cout << "Hello from from file 2" << std::endl;
+    std::cout << "Hello from from file 3" << std::endl;
 }
 
