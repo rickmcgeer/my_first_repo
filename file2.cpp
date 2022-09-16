2dd893ddb93576848148318bfbd8038b7cf8a59e
Rick McGeer <rick@mcgeer.com>
2022-09-16 03:51:38 +0000

    said goodbye

--- file2.cpp
+++ file2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "file2.h"
 void function_from_file2() {
+    std::cout << "Goodbye from from file 2" << std::endl;
     std::cout << "Hello from from file 2" << std::endl;
 }
 
